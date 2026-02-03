f52106cac451a1859709c752745ab9734268586b
Harishvardhan G <harishvardhan@example.com>
2026-02-03 13:40:32 +0530

    Setup for GitHub Pages deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "https://money-management-app-ybk0.onrender.com/api"
 });
 
 export default API;
